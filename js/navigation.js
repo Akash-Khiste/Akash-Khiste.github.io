296a830259a327bc47db21f2a178dc6e55508d1e
Akash <akash.khiste67@gmail.com>
2020-05-19 22:15:01 -0400

    m

---
+++ js/navigation.js
@@ -0,0 +1,29 @@
+
+
+
+$("#about").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#aboutdiv").offset().top
+    }, 1000);
+});
+
+$("#skills").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#skillsdiv").offset().top
+    }, 1000);
+});
+$("#workex").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#workexdiv").offset().top
+    }, 1000);
+});
+$("#projects").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#projectsdiv").offset().top
+    }, 1000);
+});
+$("#contact").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#get-in-touch").offset().top
+    }, 1000);
+});
